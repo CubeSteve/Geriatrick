0693ff145dd90185e5cdca5b7cdd60b5b7e87461
Sid <sidbevins@gmx.co.uk>
2022-05-03 16:57:57 +0100

    latest

---
+++ Geriatrick Game/Assets/StartMenu.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject startMenuCanvas;
+    [SerializeField] private GameObject uiCanvas;
+    private bool inMenu = true;
+
+    private void Start()
+    {
+        startMenuCanvas.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Return) && inMenu)
+        {
+            startMenuCanvas.SetActive(false);
+            uiCanvas.SetActive(true);
+            inMenu = false;
+        }
+    }
+}
